298008915a7140529756d47f8a12fe44663075f7
julio xavier <julio.a.x.10@gmail.com>
2019-08-16 13:46:19 -0300

    Create entities 9:30

---
+++ src/main/java/com/springcourse/domain/Request.java
@@ -0,0 +1,12 @@
+package com.springcourse.domain;
+
+import java.util.Date;
+
+public class Request {
+
+	private Long id;
+	private String subject;
+	private String description;
+	private Date criationDate;
+	
+}
