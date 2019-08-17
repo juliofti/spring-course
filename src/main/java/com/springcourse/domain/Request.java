32db2b5abc3e22012cdb49659d3519c41fc6c771
julio xavier <julio.a.x.10@gmail.com>
2019-08-17 09:56:23 -0300

    create entities complete

--- src/main/java/com/springcourse/domain/Request.java
+++ src/main/java/com/springcourse/domain/Request.java
@@ -1,12 +1,25 @@
 package com.springcourse.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
 public class Request {
 
 	private Long id;
 	private String subject;
 	private String description;
-	private Date criationDate;
+	private Date creationDate;
+	private Request state;
+	private User user;
+	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
 }
--- src/main/java/com/springcourse/domain/User.java
+++ src/main/java/com/springcourse/domain/User.java
@@ -18,6 +18,6 @@ public class User {
 	private String email;
 	private String password;
 	private List<Request> requests = new ArrayList<Request>();
-	
+	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
 }
