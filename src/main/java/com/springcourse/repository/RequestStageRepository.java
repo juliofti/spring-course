6fabce44eda9a8466f9930ded2f80bb684bf986c
julio xavier <julio.a.x.10@gmail.com>
2019-08-18 18:05:18 -0300

    Create request repository | Create request stage repository

---
+++ src/main/java/com/springcourse/repository/RequestStageRepository.java
@@ -0,0 +1,16 @@
+package com.springcourse.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.RequestStage;
+
+@Repository
+public interface RequestStageRepository extends JpaRepository<RequestStage, Long>{
+
+	
+	public List<RequestStage> findAllRequest(Long id);
+}
